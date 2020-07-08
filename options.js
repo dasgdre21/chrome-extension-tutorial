1f451257cc7a223a08439e6265e07b9c580ecf57
Dream <61371633+dasgdre21@users.noreply.github.com>
2020-07-07 18:37:15 -0700

    Add files via upload

---
+++ options.js
@@ -0,0 +1,15 @@
+let page = document.getElementById('buttonDiv');
+  const kButtonColors = ['#b5f7f7', '#eab5f7', '#b5f7b6', '#f7b5da'];
+  function constructOptions(kButtonColors) {
+    for (let item of kButtonColors) {
+      let button = document.createElement('button');
+      button.style.backgroundColor = item;
+      button.addEventListener('click', function() {
+        chrome.storage.sync.set({color: item}, function() {
+          console.log('color is ' + item);
+        })
+      });
+      page.appendChild(button);
+    }
+  }
+  constructOptions(kButtonColors);
